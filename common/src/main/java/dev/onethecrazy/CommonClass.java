9d244f1b151c857b147d55819dcbfbe5213b6e82
1TheCrazy <3thecrazy@gmail.com>
2026-01-30 17:47:37 +0100

    add dev auth + cleanup

--- common/src/main/java/dev/onethecrazy/CommonClass.java
+++ common/src/main/java/dev/onethecrazy/CommonClass.java
@@ -16,7 +16,6 @@ public class CommonClass {
     public static void init() {
 
         Constants.LOG.info("Hello from Common init on {}! we are currently in a {} environment!", Services.PLATFORM.getPlatformName(), Services.PLATFORM.getEnvironmentName());
-        Constants.LOG.info("The ID for diamonds is {}", BuiltInRegistries.ITEM.getKey(Items.DIAMOND));
 
         // It is common for all supported loaders to provide a similar feature that can not be used directly in the
         // common code. A popular way to get around this is using Java's built-in service loader feature to create
@@ -24,7 +23,6 @@ public class CommonClass {
         // we have an interface in the common code and use a loader specific implementation to delegate our call to
         // the platform specific approach.
         if (Services.PLATFORM.isModLoaded("juxtopposed-redesign")) {
-
             Constants.LOG.info("Hello to juxtopposed-redesign");
         }
     }
